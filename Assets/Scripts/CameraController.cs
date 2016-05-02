f0edadc96e29a80741bc00ac2f88e23535676045
quavngu <quangn94@gmail.com>
2016-05-02 18:15:15 +0000

    camera and some fixes

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	// Code borrowed from https://unity3d.com/learn/tutorials/projects/2d-ufo-tutorial/following-player-camera?playlist=25844
+	public GameObject player;
+	private Vector3 offset;
+
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
