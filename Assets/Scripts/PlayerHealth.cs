916c2161758d98b65c4a6d171dc97ab3b19f02b9
Ásgeir Frímannsson <Asgeir@clients-pool-1.nat.ru.is>
2016-05-03 14:38:22 +0000

    aada

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+// references: https://unity3d.com/learn/tutorials/projects/survival-shooter/player-health
+//				https://unity3d.com/learn/tutorials/modules/beginner/ui/ui-slider
+
+public class PlayerHealth : MonoBehaviour {
+
+	public Slider visualHealth;
+	public float totalHealth;
+	public float currentHealth;
+	PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+		//currentHealth = totalHealth;
+		visualHealth.value = currentHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (/*!player.underAttack && */currentHealth < totalHealth) {
+			RegenHealth ();
+		}
+	}
+
+	public void TakeDamage (int ammount) {
+		currentHealth -= ammount;
+		visualHealth.value = currentHealth;
+	}
+
+	private void RegenHealth () {
+		StartCoroutine (DoRegenHealth());
+	}
+
+	private IEnumerator DoRegenHealth () {
+		float futureHealth = currentHealth += 1;
+		while (currentHealth != futureHealth) {
+			currentHealth += 1;
+			visualHealth.value = currentHealth;
+			yield return new WaitForSeconds(2F);
+		}
+	}
+}
