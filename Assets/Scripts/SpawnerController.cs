daed0e57ea5d6b52b8aa693fff5867a4940011b9
Ásgeir Frímannsson <Asgeir@clients-pool-1.nat.ru.is>
2016-05-13 16:44:24 +0000

    sad

---
+++ Assets/Scripts/SpawnerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerController : UnitController {
+
+	bool spawned;
+	public int numSpiders;
+	public float delay;
+
+	GameObject spider;
+
+	// Use this for initialization
+	void Start () {
+		spawned = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!spawned) {
+			if (distance < maxRange) {
+				StartCoroutine (StartSpawning ());
+				spawned = true;
+			}
+		}
+	}
+
+	IEnumerator StartSpawning() {
+		int i = 0;
+		while (i < numSpiders) {
+			GameObject newBat = Instantiate (spider);
+			newBat.transform.position = this.transform.position;
+			yield return new WaitForSeconds (delay);
+			i++;
+		}
+	}
+}
