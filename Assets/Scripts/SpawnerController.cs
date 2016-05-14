040fe5dcaf89092ae016562aa6cbc8d5eaa273fa
Ásgeir Frímannsson <Asgeir@clients-pool-1.nat.ru.is>
2016-05-14 16:57:31 +0000

    level 2

--- Assets/Scripts/SpawnerController.cs
+++ Assets/Scripts/SpawnerController.cs
@@ -1,43 +1,67 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SpawnerController : UnitController {
+public class SpawnerController : MonoBehaviour {
 
 	bool spawned;
+	bool musicPlayed;
 	public int numSpiders;
 	public float delay;
+	private int numSpawned;
+	private bool musicSwapped;
 
 	public GameObject spider;
 
 	// Use this for initialization
 	void Start () {
-		base.Start ();
+		musicPlayed = false;
 		spawned = false;
-		timeSlowed = true;
-		word = "";
-		wordDone = word;
-		wordLeft = word;
+		numSpawned = 0;
+		musicSwapped = false;
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		base.Update ();
+		if (numSpawned == numSpiders && musicSwapped == false) {
+			musicSwapped = true;
+			StartCoroutine (MusicBack ());
+			GameObject.Find ("PanicDoorOpen").transform.position = new Vector3 (1000, -6, 0);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.name != "Player") {
+			return;
+		}
 		if (!spawned) {
-			if (distance < maxRange) {
-				StartCoroutine (StartSpawning ());
-				spawned = true;
-			}
+			GameObject.Find ("PanicDoorClose").transform.position = new Vector3 (-25.5F, -6, 0);
+			GameObject.Find ("Audio Source").GetComponent<AudioSource> ().Stop();
+			GameObject.Find ("Scream").GetComponent<AudioSource> ().Play();
+			GameObject.Find ("Danger Music").GetComponent<AudioSource> ().Play();
+			musicPlayed = true;
+			StartCoroutine (StartSpawning ());
+			spawned = true;
 		}
 	}
 
 	IEnumerator StartSpawning() {
 		int i = 0;
 		while (i < numSpiders) {
+			string temp = "Spawn" + (i % 3);
+			Debug.Log (temp);
 			GameObject newSpider = Instantiate (spider);
-			newSpider.transform.position = this.transform.position;
-			Debug.Log ("SPAWNING");
+			newSpider.transform.position = GameObject.Find(temp).transform.position;
+			SpiderController ctrl = newSpider.GetComponent<SpiderController> ();
+			ctrl.maxRange = 30;
+			ctrl.damage = 2;
 			yield return new WaitForSeconds (delay);
 			i++;
+			numSpawned++;
 		}
+	}
+
+	IEnumerator MusicBack () {
+		yield return new WaitForSeconds (2F);
+		GameObject.Find ("Audio Source").GetComponent<AudioSource> ().Play();
+		GameObject.Find ("Danger Music").GetComponent<AudioSource> ().Stop();
 	}
 }
