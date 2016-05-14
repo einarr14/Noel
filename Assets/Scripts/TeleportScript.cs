11f788473c884de1f5ad399a14cbaf771c1bced9
einarr14 <einarr14@ru.is>
2016-05-14 17:39:45 +0000

    Level 3 portals completed

---
+++ Assets/Scripts/TeleportScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        string destination;
+
+        switch(this.name)
+        {
+            case "TeleporterIn":
+            destination = "TeleporterOut";
+            break;
+            case "TeleporterIn2":
+            destination = "TeleporterOut3";
+            break;
+            case "TeleporterIn3":
+            destination = "TeleporterOut6";
+            break;
+            case "TeleporterIn4":
+            destination = "TeleporterOut5";
+            break;
+            case "TeleporterIn5":
+            destination = "TeleporterOut2";
+            break;
+            case "TeleporterIn6":
+            destination = "TeleporterOut4";
+            break;
+            default:
+            destination = "TeleporterOut7";
+            break;
+        }
+            
+        other.transform.position = GameObject.Find(destination).transform.position;
+    }
+    
+}
